02e11820f831f1fcd7f248cf57e62f7d449b654f
dsvhub <58570613+dsvhub@users.noreply.github.com>
2025-11-24 16:40:07 -0500

    Add AppLinked_2.0.5).apk

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ def public_page():
         c = conn.cursor()
         c.execute("SELECT filename, downloads, filesize, uploaded_at FROM apks")
         apks = c.fetchall()
-    return render_template('Html editor.py', apks=apks, version=APP_VERSION)
+    return render_template('public_apks.html', apks=apks, version=APP_VERSION)
 
 @app.route('/downloads/<filename>')
 def download_apk(filename):
